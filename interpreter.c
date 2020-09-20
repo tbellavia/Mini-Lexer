4f3c91746cf6eac8d27d7bb184093d6e5f9e57b4
Tony Bellavia <bbellavi@student.42.fr>
2020-09-20 23:32:41 +0200

    Add multi-digit number support

--- interpreter.c
+++ interpreter.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 03:24:12 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/09/20 21:54:59 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/09/20 23:32:14 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,34 @@ Token_t		get_next_token(Interpreter_t *inter)
 {
 	const size_t	len = strlen(inter->input);
 	char			current_char;
+	Token_t			tok;
 
 	if (inter->index > len - 1)
 		return ((Token_t){_EOF, 0});
 
-	current_char = inter->input[inter->index];
 
-	inter->index++;
-
+	// skip whitespaces
 	while (isspace(inter->input[inter->index]) && inter->index < len - 1)
 		inter->index++;
 
+	current_char = inter->input[inter->index];
+	
 	if (isdigit(current_char))
-		return ((Token_t){INT, current_char - '0'});
+	{
+		tok = (Token_t){INT, atoi(&inter->input[inter->index])};
+		// skip digits so we don't evaluate next digit if number has multi digits
+		while (isdigit(inter->input[inter->index]) && inter->index < len - 1)
+			inter->index++;
+		return (tok);
+	}
 	else if (current_char == '+')
-		return ((Token_t){OP, '+'});
+		tok = (Token_t){OP, '+'};
+	else
+		tok = (Token_t){ERR, 0};
+	
+	inter->index++;
 
-	return ((Token_t){ERR, 0});
+	return (tok);
 }
 
 int		eat(Interpreter_t *inter, int token_type)
